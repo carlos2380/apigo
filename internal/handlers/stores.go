994069785753e7a2e407ed56a2adfdb6f13baa3b
carlos2380 <carlos.2380@hotmail.com>
2021-08-06 23:52:57 +0200

    basic CRUD stores

--- internal/handlers/stores.go
+++ internal/handlers/stores.go
@@ -3,14 +3,68 @@ package handlers
 import (
 	"apigo/api"
 	"encoding/json"
+	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 var stores []api.Store
 
+func GetStores(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(stores)
+}
+
 func GetStore(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	stores = append(stores, api.Store{ID: "1", Name: "Store1"})
-	stores = append(stores, api.Store{ID: "2", Name: "Store2"})
+
+	params := mux.Vars(r)
+	id := params["id"]
+	for _, item := range stores {
+		if item.ID == id {
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("Item not found"))
+}
+
+func DeleteStore(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	id := params["id"]
+	for i, store := range stores {
+		if store.ID == id {
+			stores = append(stores[:i], stores[i+1:]...)
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(stores)
+			return
+		}
+	}
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("Item not found"))
+}
+
+func PostStore(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var store api.Store
+	err := json.NewDecoder(r.Body).Decode(&store)
+	if err != nil {
+		fmt.Println("error")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("error"))
+		return
+	}
+
+	stores = append(stores, store)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(stores)
+}
+
+func PutStore(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(stores)
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -13,7 +13,12 @@ type Server struct {
 
 func NewServer() *Server {
 	r := mux.NewRouter()
-	r.HandleFunc("/api/stores", handlers.GetStore).Methods("GET")
+
+	r.HandleFunc("/api/stores", handlers.GetStores).Methods(http.MethodGet)
+	r.HandleFunc("/api/stores/{id}", handlers.GetStore).Methods(http.MethodGet)
+	r.HandleFunc("/api/stores/{id}", handlers.DeleteStore).Methods(http.MethodDelete)
+	r.HandleFunc("/api/stores", handlers.PostStore).Methods(http.MethodPost)
+	r.HandleFunc("/api/stores/{id}", handlers.PutStore).Methods(http.MethodPut)
 
 	return &Server{
 		Router: r,
