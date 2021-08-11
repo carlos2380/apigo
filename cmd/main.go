03eaeb84a8a826ea9f67e7f1c753bc9134935c75
carlos2380 <carlos.2380@hotmail.com>
2021-08-11 04:53:03 +0200

    add main to commit

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"apigo/internal/server"
+	"apigo/internal/storage"
 	"apigo/internal/storage/postgres"
 	"flag"
 	"log"
@@ -10,9 +11,23 @@ import (
 
 func main() {
 	port := flag.String("port", "8000", "Port the server will be listening")
+	dbDriver := flag.String("driver", "", "Database driver (postgres only supported now)")
 	flag.Parse()
-	ctrlDB := &postgres.PostgresDB{}
-	server := server.NewServer(ctrlDB)
+
+	var storage storage.DataBase
+	var err error
+	switch *dbDriver {
+	case "postgres":
+		storage, err = postgres.NewPostgresStorage()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer storage.(*postgres.PostgresDB).CloseDB()
+	default:
+		log.Fatalf("Unsupported driver %s", *dbDriver)
+	}
+
 	log.Printf("Listening on 0.0.0.0:%s", *port)
-	log.Fatal(http.ListenAndServe(":"+*port, server.Router))
+	log.Fatal(http.ListenAndServe(":"+*port, server.NewRouter(storage)))
 }
